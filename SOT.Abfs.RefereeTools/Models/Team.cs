852dfff4be060cadd0d2a6d06c371dd5b802daa8
Adrien Clerbois <adrien@senseoftech.be>
2020-08-22 12:03:48 +0200

    Add teams page with anonymous calculation fees

---
+++ SOT.Abfs.RefereeTools/Models/Team.cs
@@ -0,0 +1,16 @@
+﻿namespace SOT.Abfs.RefereeTools.Models
+{
+    public class Team
+    {
+        public string Division { get; set; }
+        public int Matricule { get; set; }
+        public string ClubName { get; set; }
+        public string Hall { get; set; }
+        public int HallId { get; set; }
+        public int Bloc1 { get; set; }
+        public int Bloc2 { get; set; }
+        public int Hour { get; set; }
+        public int? Difference { get; set; }
+        public string SearchString => $"{Division}|{Matricule}|{ClubName}|{Hall}|{HallId}|{Bloc1}{Bloc2}|{Hour}|{Bloc1}-{Bloc2}";
+    }
+}
---
+++ SOT.Abfs.RefereeTools/Models/TeamsInformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOT.Abfs.RefereeTools.Models
+{
+
+    public class TeamsInformation
+    {
+        public DateTime LastUpdate { get; set; }
+        public Team[] Teams { get; set; }
+    }
+
+}
